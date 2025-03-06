bb99c2b905e45bc81fabca5a8749a58a54ccaf34
czh <1833441703@qq.com>
2025-03-06 17:46:55 +0800

    画出蛇以及用够使用键盘输入

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,75 +1,79 @@
 import { AcGameObject } from "./AcGameObject";
 import { Wall } from "./Wall";
-
+import { Snake } from './Snake';
 
 export class GameMap extends AcGameObject {
-    constructor(ctx, parent){
-        super(); // 执行地图的构造函数之前先执行父类的构造函数
+    constructor(ctx, parent) {
+        super();
 
         this.ctx = ctx;
-        this.parent = parent; 
+        this.parent = parent;
         this.L = 0;
-        
-        this.rows = 13;
-        this.cols = 13;
 
+        this.rows = 13;
+        this.cols = 14;
+        
+        this.inner_walls_count = 20;
         this.walls = [];
-        this.inner_walls_count = 30;
+
+        this.snakes = [
+            new Snake({id: 0, color: "#4876EC", r: this.rows - 2, c: 1}, this),
+            new Snake({id: 1, color: "#F94848", r: 1, c: this.cols - 2}, this),
+        ];
     }
 
-    // 使用flood fill算法计算地图中两名玩家的是否是联通的
-    check_connectivity(g, sx, sy, tx, ty){
-        if (sx ==tx && sy == ty) return true;
+    check_connectivity(g, sx, sy, tx, ty) {
+        if (sx == tx && sy == ty) return true;
         g[sx][sy] = true;
 
-        let dx = [-1, 0, 1, 0], dy= [0,1,0,-1];
-        for(let i = 0; i< 4 ;i++){
-            let x = sx +dx[i], y = sy + dy[i];
-            if(!g[x][y] && this.check_connectivity(g,x,y,tx,ty))
+        let dx = [-1, 0, 1, 0], dy = [0, 1, 0, -1];
+        for (let i = 0; i < 4; i ++ ) {
+            let x = sx + dx[i], y = sy + dy[i];
+            if (!g[x][y] && this.check_connectivity(g, x, y, tx, ty))
                 return true;
         }
+
         return false;
     }
 
-
     create_walls() {
         const g = [];
-        for (let r = 0; r < this.rows; r++) {
+        for (let r = 0; r < this.rows; r ++ ) {
             g[r] = [];
-            for (let c = 0; c < this.cols; c++) {
-                g[r][c] = false; 
+            for (let c = 0; c < this.cols; c ++ ) {
+                g[r][c] = false;
             }
         }
+
         // 给四周加上障碍物
-        for (let r = 0; r < this.rows; r++) {
+        for (let r = 0; r < this.rows; r ++ ) {
             g[r][0] = g[r][this.cols - 1] = true;
         }
-        
-        for (let c = 0; c < this.cols; c++) {
+
+        for (let c = 0; c < this.cols; c ++ ) {
             g[0][c] = g[this.rows - 1][c] = true;
         }
 
-
         // 创建随机障碍物
-        for (let i = 0; i < this.inner_walls_count /2; i++){
-            for (let j =0; j<1000; j++){
+        for (let i = 0; i < this.inner_walls_count / 2; i ++ ) {
+            for (let j = 0; j < 1000; j ++ ) {
                 let r = parseInt(Math.random() * this.rows);
                 let c = parseInt(Math.random() * this.cols);
-                if (g[r][c] || g[c][r]) continue;
-                if (r == this.rows -2 && c==1 || r==1 && c == this.cols -2)
+                if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue;
+                if (r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2)
                     continue;
-                g[r][c]=g[c][r]=true;
+
+                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
                 break;
             }
         }
 
-        // 复制地图
         const copy_g = JSON.parse(JSON.stringify(g));
+        if (!this.check_connectivity(copy_g, this.rows - 2, 1, 1, this.cols - 2))
+            return false;
 
-        if (!this.check_connectivity(copy_g, this.rows - 2, 1, 1, this.cols - 2)) return false;
-
-        for (let r = 0; r < this.rows; r++) {
-            for (let c = 0; c < this.cols; c++) {
+        for (let r = 0; r < this.rows; r ++ ) {
+            for (let c = 0; c < this.cols; c ++ ) {
                 if (g[r][c]) {
                     this.walls.push(new Wall(r, c, this));
                 }
@@ -79,38 +83,89 @@ export class GameMap extends AcGameObject {
         return true;
     }
 
-    // 这是一个游戏对象
-    start(){
-        for (let i = 0; i < 1000; i ++) {
+    add_listening_events() {
+        this.ctx.canvas.focus();
+
+        const [snake0, snake1] = this.snakes;
+        this.ctx.canvas.addEventListener("keydown", e => {
+            if (e.key === 'w') snake0.set_direction(0);
+            else if (e.key === 'd') snake0.set_direction(1);
+            else if (e.key === 's') snake0.set_direction(2);
+            else if (e.key === 'a') snake0.set_direction(3);
+            else if (e.key === 'ArrowUp') snake1.set_direction(0);
+            else if (e.key === 'ArrowRight') snake1.set_direction(1);
+            else if (e.key === 'ArrowDown') snake1.set_direction(2);
+            else if (e.key === 'ArrowLeft') snake1.set_direction(3);
+        });
+    }
+
+    start() {
+        for (let i = 0; i < 1000; i ++ ) 
             if (this.create_walls())
                 break;
-        }
         
+        this.add_listening_events();
     }
 
     update_size() {
         this.L = parseInt(Math.min(this.parent.clientWidth / this.cols, this.parent.clientHeight / this.rows));
-        this.ctx.canvas.width = this.cols * this.L;
-        this.ctx.canvas.height = this.rows * this.L;
+        this.ctx.canvas.width = this.L * this.cols;
+        this.ctx.canvas.height = this.L * this.rows;
     }
 
-    update(){
-        this.update_size(); // 出了第一��之外，每一��执行一次
-        this.render(); // 每一帧执行一次
+    check_ready() {  // 判断两条蛇是否都准备好下一回合了
+        for (const snake of this.snakes) {
+            if (snake.status !== "idle") return false;
+            if (snake.direction === -1) return false;
+        }
+        return true;
     }
 
-    render() { // 渲染
-        // 假设地图是用 Canvas 画的，并且每一格用一个方格表示
-        const color_even = "#AAD751", color_odd = "#A2D048";
-        for (let i = 0; i < this.rows; i++) {
-            for (let j = 0; j < this.cols; j++) {
-                this.ctx.fillStyle = (i + j) % 2 === 0? color_even : color_odd;
-                this.ctx.fillRect(j * this.L, i * this.L, this.L, this.L);
-            }
+    next_step() {  // 让两条蛇进入下一回合
+        for (const snake of this.snakes) {
+            snake.next_step();
         }
     }
 
+    check_valid(cell) {  // 检测目标位置是否合法：没有撞到两条蛇的身体和障碍物
+        for (const wall of this.walls) {
+            if (wall.r === cell.r && wall.c === cell.c)
+                return false;
+        }
+
+        for (const snake of this.snakes) {
+            let k = snake.cells.length;
+            if (!snake.check_tail_increasing()) {  // 当蛇尾会前进的时候，蛇尾不要判断
+                k -- ;
+            }
+            for (let i = 0; i < k; i ++ ) {
+                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    update() {
+        this.update_size();
+        if (this.check_ready()) {
+            this.next_step();
+        }
+        this.render();
+    }
+
+    render() {
+        const color_even = "#AAD751", color_odd = "#A2D149";
+        for (let r = 0; r < this.rows; r ++ ) {
+            for (let c = 0; c < this.cols; c ++ ) {
+                if ((r + c) % 2 == 0) {
+                    this.ctx.fillStyle = color_even;
+                } else {
+                    this.ctx.fillStyle = color_odd;
+                }
+                this.ctx.fillRect(c * this.L, r * this.L, this.L, this.L);
+            }
+        }
+    }
 }
-
-
-

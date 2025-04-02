96eb6fe85c05dd96a2b4c3b9b1fc32ac5cd09723
czh <1833441703@qq.com>
2025-04-02 17:33:59 +0800

    bot的增删改查

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,14 +7,14 @@
 import NavBar from './components/NavBar.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import $ from 'jquery'
+// import $ from 'jquery'
 
 export default {
 
   components: {
     NavBar
   },
-  setup() {
+  // setup() {
     // $.ajax({
     //   url: "http://127.0.0.1:3000/user/account/token/",
     //   type: 'POST',
@@ -48,23 +48,23 @@ export default {
     //   }
     // });
 
-    $.ajax({
-      url: "http://127.0.0.1:3000/user/account/register/",
-      type: 'post',
-      data: {
-        username: 'yxc2',
-        password: 'yxc2',
-        confirmedPassword: 'yxc2',
-      },
-      success(resp) {
-        console.log(resp);
-      },
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/register/",
+    //   type: 'post',
+    //   data: {
+    //     username: 'yxc2',
+    //     password: 'yxc2',
+    //     confirmedPassword: 'yxc2',
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
 
-      error(resp) {
-        console.log(resp);
-      }
-    });
-  }
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // });
+  // }
 }
   
 
--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -1,15 +1,55 @@
 <template>
-    <ContentField>我的Bot
+    <ContentField>
+        我的Bot
     </ContentField>
 </template>
 
 
 <script>
 import ContentField from '@/components/ContentField.vue'
+import $ from 'jquery'
+import { useStore } from 'vuex'
+
 
 export default {
     components: {
         ContentField
+    },
+    setup() {
+        const store = useStore();
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/add/",
+        //     type: "POST",
+        //     data: {
+        //         title: "我的Bot标题",
+        //         description: "Bot的描述",
+        //         content: "Bot的描述",
+        //     },
+            
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token, // 使用Vuex中的token
+        //     },
+        //     success(resp){
+        //         console.log(resp);
+        //     },
+        //     error(resp){
+        //         console.log(resp); 
+        //     }
+        // })
+        $.ajax({
+            url: "http://127.0.0.1:3000/user/bot/getlist/",
+            type: "get",
+            
+            headers: {
+                Authorization: "Bearer " + store.state.user.token, // 使用Vuex中的token
+            },
+            success(resp){
+                console.log(resp);
+            },
+            error(resp){
+                console.log(resp); 
+            }
+        })
     }
 }
 </script>
